b99dc1ab3ff3d1e6a68080b08ed0604a989da09d
jero-peter <57021258+jero-peter@users.noreply.github.com>
2023-04-13 12:49:17 +0530

    animationdone

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,25 @@ export default {
       spacing: {
         '24': '6rem',
       },
+      keyframes: {
+        floaty: {
+          '0%': { transform: 'translateY(100%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+        floatx: {
+          '0%': { transform: 'translateX(-100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+        fadein: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+      animation: {
+        floaty: 'floaty 1s ease-in-out forwards',
+        floatx: 'floatx 1s ease-in-out forwards',
+        fadein: 'fadein 1s ease-in-out forwards',
+      },
     },
   },
   plugins: [],
